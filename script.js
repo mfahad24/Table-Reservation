1bc3d0120d13a195744dd854ca36b24ec6874c5a
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-02-06 21:03:01 -0500

    all done but hover - test media query

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-$(document).ready(function() { //anonymous function is a no name function without parameters. can also be written as
-  //$(function () {});
+$(document).ready(function() {
+
+  var tableDiv;
+
   $(".available").click(function() {
-    $("#inputdiv").css("display", "block");
-    $tablenumber = $(event.target).text()
+    tableDiv = $(this);
+    $("#inputdiv").fadeIn(1000).css("display", "block");
+    $tablenumber = $(event.target).text();
     $(".num").html($tablenumber);
-    $(event.target).removeClass("available");
+
   });
 
   $(".xout").click(function() {
-    $("#inputdiv").css("display", "none");
+    $("#inputdiv").fadeOut(500);
+
   });
-  //unable to find code to revert reserved to available after entering
-  //the input field div and X'ing out
 
   $(".btn").click(function(event) {
     event.preventDefault();
-    $("#inputdiv").css("display", "none");
+    $("#inputdiv").fadeOut(500);
+    var tablenumber = $(".num").text();
+    $(".available:contains(" + tablenumber + ")").removeClass("available").addClass("reserved");
+    hoverData();
+    $("#name").val("");
+    $("#guests").val("");
+    // $(".available)".removeClass("reserved");
   });
 
-
+  function hoverData () {
+    var name = $("#name").val();
+    var guestSize = $("#guests").val();
+    tableDiv.append('<div id="hoverdiv" class="hover"><div>Name: ' + name + '</div><div>Guests: ' + guestSize + '</div></div>');
+  }
 
 });
+
+//submit vs click .... submit you can use enter to submit it and not have to click the button
+
+//anonymous function is a no name function without parameters. can also be written as $(function () {});
